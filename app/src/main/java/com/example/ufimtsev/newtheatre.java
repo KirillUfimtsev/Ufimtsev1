e36b87d75018113acce87cdb07688bae4b7fb766
Kirill Ufimtsev <dufick69@gmail.com>
2022-05-29 20:00:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ufimtsev/newtheatre.java
@@ -0,0 +1,14 @@
+package com.example.ufimtsev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newtheatre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newtheatre);
+    }
+}
